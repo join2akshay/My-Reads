ab4e1e66219dc87a20dd3313d0971e59c84767ed
tvibhu12 <tvibhu12@gmail.com>
2019-07-12 00:10:38 +0530

    Updates

---
+++ precache-manifest.0b5bf5945992c53bbc337014a7ab7bc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a04f3b06d2578276525",
+    "url": "/My-Reads/static/css/main.bf529e0f.chunk.css"
+  },
+  {
+    "revision": "7a04f3b06d2578276525",
+    "url": "/My-Reads/static/js/main.7a04f3b0.chunk.js"
+  },
+  {
+    "revision": "ddc95911975fce9dbe58",
+    "url": "/My-Reads/static/js/1.ddc95911.chunk.js"
+  },
+  {
+    "revision": "6577964ed44e9d458ff4",
+    "url": "/My-Reads/static/js/runtime~main.6577964e.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/My-Reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/My-Reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/My-Reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "67131d5998cf04ad15a5f9b6f01708ab",
+    "url": "/My-Reads/index.html"
+  }
+];
